5beb917e917d136cd7e794d5ec194e6a05a0df5b
unknown <talhaaftab661@gmail.com>
2021-10-12 12:55:22 +0500

    initial emailing

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const mailsRoute = require("./routes/mails");
+
+require("dotenv/config");
+
+app.use(bodyParser.json());
+//Importing routes
+app.use("/mail", mailsRoute);
+//on page routing
+app.get("/", (req, res) => {
+  res.send("We Are On Home");
+});
+
+app.listen(3000);
---
+++ routes/mails.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const nodemailer = require("nodemailer");
+//For the mailing system
+const transporter = nodemailer.createTransport({
+  host: "binarymarvels.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "testemails@binarymarvels.com",
+    pass: "malik1997",
+  },
+});
+//For the posting the content on the server
+router.post("/", async (req, res) => {
+  const from = req.body.mail;
+  const subject = req.body.subject;
+  const text = req.body.text;
+  try {
+    const options = {
+      from,
+      to: "info@binarymarvels.com",
+      subject,
+      text,
+    };
+    transporter.sendMail(options, function (err, info) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          message: err,
+        });
+      }
+      res.status(200).json({
+        message: info.response,
+      });
+    });
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+module.exports = router;
