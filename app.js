23ba70a75e950a78bd575e077271bc5f0fae81ec
unknown <talhaaftab661@gmail.com>
2021-10-14 13:53:46 +0500

    changing the localhost

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const mailsRoute = require("./routes/mails");
-
+const cors = require("cors");
 require("dotenv/config");
-
+// cors for the runnnig the localhost
+app.use(cors()); // Use this after the variable declaration
 app.use(bodyParser.json());
 //Importing routes
 app.use("/mail", mailsRoute);
 //on page routing
 app.get("/", (req, res) => {
-  res.send("We Are On Home");
+	res.send("We Are On Home");
 });
 
-app.listen(process.env.PORT||3000);
+app.listen(process.env.PORT || 3000);
