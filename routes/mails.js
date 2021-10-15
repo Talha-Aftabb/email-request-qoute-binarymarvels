1a65a75dc4446030dc2278bf3c157978fd8ee05d
unknown <talhaaftab661@gmail.com>
2021-10-15 17:02:58 +0500

    adding the validation

--- routes/mails.js
+++ routes/mails.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
+const { check, validationResult } = require("express-validator");
 //For the mailing system
 const transporter = nodemailer.createTransport({
 	host: "binarymarvels.com",
@@ -12,34 +13,53 @@ const transporter = nodemailer.createTransport({
 	},
 });
 //For the posting the content on the server
-router.post("/", async (req, res) => {
-	const from = req.body.mail;
-	const name = req.body.name;
-	const email = req.body.email;
-	const phone = req.body.phone;
-	const message = req.body.message;
+router.post(
+	"/",
+	[
+		check("mail").notEmpty().withMessage("Mail cannot be empty"),
+		check("name").notEmpty().withMessage("Name cannot be empty"),
+		check("email").notEmpty().withMessage("Email cannot be empty"),
+		check("phone")
+			.notEmpty()
+			.withMessage("Phone Number cannot be empty")
+			.isLength({ min: 11 })
+			.withMessage("phone number must be atleast 11 characters"),
+		check("message").notEmpty().withMessage("Message cannot be empty"),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		console.log(errors);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		const from = req.body.mail;
+		const name = req.body.name;
+		const email = req.body.email;
+		const phone = req.body.phone;
+		const message = req.body.message;
 
-	try {
-		const options = {
-			from: "testemails@binarymarvels.com",
-			to: ["malikmusa1997@gmail.com", "info@binarymarvels.com", from],
-			subject: "Message from binarymarvels ✔", // Subject line
-			html: `<b>name </b> : ${name} </br> <b>email </b> : ${email} </br> <b>phone No </b> : ${phone} </br> <b>message </b> : ${message} `, // html body
-		};
-		transporter.sendMail(options, function (err, info) {
-			if (err) {
-				console.log(err);
-				return res.status(500).json({
-					message: err,
+		try {
+			const options = {
+				from: "testemails@binarymarvels.com",
+				to: ["malikmusa1997@gmail.com", "info@binarymarvels.com", from],
+				subject: "Message from binarymarvels ✔", // Subject line
+				html: `<b>name </b> : ${name} </br> <b>email </b> : ${email} </br> <b>phone No </b> : ${phone} </br> <b>message </b> : ${message} `, // html body
+			};
+			transporter.sendMail(options, function (err, info) {
+				if (err) {
+					console.log(err);
+					return res.status(500).json({
+						message: err,
+					});
+				}
+				res.status(200).json({
+					message: info.response,
 				});
-			}
-			res.status(200).json({
-				message: info.response,
 			});
-		});
-	} catch (err) {
-		res.json({ message: err });
+		} catch (err) {
+			res.json({ message: err });
+		}
 	}
-});
+);
 
 module.exports = router;
