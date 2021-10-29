16849dfbc8b97fb26a918e7ad6c47f430e900be7
unknown <talhaaftab661@gmail.com>
2021-10-29 15:11:20 +0500

    Adding the paragraph for the user

--- routes/mails.js
+++ routes/mails.js
@@ -54,7 +54,15 @@ router.post(
 				from: "testemails@binarymarvels.com",
 				to: [from],
 				subject: "Message from binarymarvels ✔", // Subject line
-				html: `<p></p>`, // html body
+				html: `<p>Hello ${name}, hope you are doing great.
+
+				Thanks for requesting a free consultation, we have received your request and one of our representator will contact you soon.
+				
+				In the meantime you can know more about us by downloading the PDF below
+				
+				Regards;
+				Binary Marvels Team
+				</p>`, // html body
 				attachments: [
 					{
 						filename: "attachment.pdf",
