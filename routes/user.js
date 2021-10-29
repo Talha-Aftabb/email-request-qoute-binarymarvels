a08820349b1877d7041fd7a5876d75a816c14d4e
unknown <talhaaftab661@gmail.com>
2021-10-29 17:06:39 +0500

    last2

--- routes/user.js
+++ routes/user.js
@@ -33,11 +33,11 @@ router.post(
 				from: "testemails@binarymarvels.com",
 				to: [email],
 				subject: "Message from binarymarvels ✔", // Subject line
-				html: `<p>Hello ${name}, hope you are doing great. <br/> <br/>
-				Thanks for requesting a free consultation, we have received your request and one of our representator will contact you soon. <br>
-				In the meantime you can know more about us by downloading the PDF below <br>
-				Regards; <br>
-				Binary Marvels Team <br>
+				html: `<p>Hello ${name}, hope you are doing great. <br/> 
+				Thanks for requesting a free consultation, we have received your request and one of our representator will contact you soon. <br/>
+				In the meantime you can know more about us by downloading the PDF below <br/>
+				Regards; <br/>
+				Binary Marvels Team <br/>
 				</p>`, // html body
 				attachments: [
 					{
